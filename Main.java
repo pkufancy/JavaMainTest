9dcbd392f9bc726c514ccdaf161a1af8d1904b23
xfan <xing_fan@dataagility.com>
2019-08-28 12:33:38 +1000

    Jenkins test mainTest

--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@ public class Main {
         boolean result = "a".equals(s);
         //YYYY[MM[DD[HH[MM[SS[.S[S[S[S]]]]]]]]][+/-ZZZZ].
         //s = s.replaceAll("([|^&])(\\d{4,14})(\\.\\d{1,4})?\\+1[10][30]0([|^&])", "$1$2$3$4");
-        System.out.println(result);
+        System.out.println("SUCCESS");
     }
 }
