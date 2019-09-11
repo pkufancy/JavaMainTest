f74eda00bed0a7babeb0e78f1e2aa2f06b6b574e
xfan <xing_fan@dataagility.com>
2019-09-12 09:46:46 +1000

    JIRATest

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         String s = null;
         boolean result = "a".equals(s);
+        
         //YYYY[MM[DD[HH[MM[SS[.S[S[S[S]]]]]]]]][+/-ZZZZ].
         //s = s.replaceAll("([|^&])(\\d{4,14})(\\.\\d{1,4})?\\+1[10][30]0([|^&])", "$1$2$3$4");
         System.out.println("SUCCESS");
