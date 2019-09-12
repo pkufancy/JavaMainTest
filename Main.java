2661b646c8ca10c828e53336d954e779cc787222
xfan <xing_fan@dataagility.com>
2019-09-12 13:48:02 +1000

    AnotherJiraTest

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
         boolean result = "a".equals(s);
         //Meaningless line
         //test changes
+        //no format = %m
         //YYYY[MM[DD[HH[MM[SS[.S[S[S[S]]]]]]]]][+/-ZZZZ].
         //s = s.replaceAll("([|^&])(\\d{4,14})(\\.\\d{1,4})?\\+1[10][30]0([|^&])", "$1$2$3$4");
         System.out.println("SUCCESS");
